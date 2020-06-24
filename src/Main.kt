256c94c61f9a39789860d882017d7fe4581455c3
alex <astrgan@gmail.com>
2020-06-24 16:24:37 +1000

    add

--- src/Main.kt
+++ src/Main.kt
@@ -1,7 +1,15 @@
-
+var list = mutableListOf<int>()
 
 fun main(args : Array<String>) {
     println("Введите число:")
     var input = readLine()
+
+    for ()
+
     println("Вы ввели: $input")
+}
+
+fun addNumToList(num:int):MutableList<int>{
+    list.add(num)
+    return list
 }
